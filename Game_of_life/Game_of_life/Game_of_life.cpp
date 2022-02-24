f07b9a2e1e59fb170835f6031880f7d65aa8157c
Viktor <vik.kostyrin@gmail.com>
2022-02-24 18:32:12 +0300

    add class Blinker, Glider Dot and  RandomField; also add func Set Window

--- Game_of_life/Game_of_life/Game_of_life.cpp
+++ Game_of_life/Game_of_life/Game_of_life.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <cstdlib>
+#include <Windows.h>
 
-#define HEIGHT 21
-#define WIDTH 50
-#define FPS 600
+
+#define HEIGHT 30
+#define WIDTH 100
+
+#define FPS 100
+
+
+void SetWindow(int Width, int Height) {
+    _COORD coord;
+    coord.X = Width;
+    coord.Y = Height;
+    _SMALL_RECT Rect;
+    Rect.Top = 0;
+    Rect.Left = 0;
+    Rect.Bottom = Height - 1;
+    Rect.Right = Width - 1;
+    HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleScreenBufferSize(Handle, coord);
+    SetConsoleWindowInfo(Handle, TRUE, &Rect);
+}
 
 class Shape {
 public:
@@ -15,17 +34,46 @@ public:
     char** figure;
 };
 
+class Glider : public Shape {
+    static const int GLIDER_SIZE = 3;
+public:
+    Glider(int x, int y);
+    ~Glider();
+};
+
+class Dot : public Shape {
+    static const int DOT_HEIGHT = 1;
+    static const int DOT_WIDTH = 1;
+public:
+    Dot(int x, int y);
+    ~Dot();
+};
+
+class Blinker : public Shape {
+    static const int BLINKER_HEIGHT = 3;
+    static const int BLINKER_WIDTH = 1;
+public:
+    Blinker(int x, int y);
+    ~Blinker();
+};
+
+class RandomField : public Shape {
+    static const int RandomField_HEIGHT = HEIGHT;
+    static const int RandomField_WIDTH = WIDTH;
+public:
+    RandomField();
+    ~RandomField();
+};
 
 class GameOfLife {
     char world[HEIGHT][WIDTH];
     char otherWorld[HEIGHT][WIDTH];
     bool toggle;
-
+    char getState(char state, int xCoord, int yCoord);
+    void update();
 public:
     GameOfLife();
     void print();
-    void update();
-    char getState(char state, int xCoord, int yCoord);
     void run();
     void add(Shape sh);
 };
@@ -51,10 +99,8 @@ GameOfLife::GameOfLife() {
 }
 
 void GameOfLife::print() {
-    for (int i = 0; i < WIDTH; i++) {
-        std::cout << '=';
-    }
-    std::cout << std::endl;
+    
+    
     if (toggle) {
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
@@ -71,10 +117,8 @@ void GameOfLife::print() {
             std::cout << std::endl;
         }
     }
-    for (int i = 0; i < WIDTH; i++) {
-        std::cout << '=';
-    }
-    std::cout << std::endl;
+    
+   
 }
 
 void GameOfLife::update() {
@@ -137,7 +181,7 @@ char GameOfLife::getState(char state, int y, int x) {
 }
 
 void GameOfLife::run() {
-    while(true) {
+    for(;;) {
         system("cls");
         print();
         std::this_thread::sleep_for(std::chrono::milliseconds(FPS));
@@ -145,11 +189,111 @@ void GameOfLife::run() {
     }
 }
 
+Glider::Glider(int x, int y) {
+    xCoord = x;
+    yCoord = y;
+    height = GLIDER_SIZE;
+    width = GLIDER_SIZE;
+    figure = new char* [GLIDER_SIZE];
+    for (int i = 0; i < GLIDER_SIZE; i++) {
+        figure[i] = new char[GLIDER_SIZE];
+    }
+    for (int i = 0; i < GLIDER_SIZE; i++) {
+        for (int j = 0; j < GLIDER_SIZE; j++) {
+            figure[i][j] = '.';
+        }
+    }
+    figure[0][1] = 'X';
+    figure[1][2] = 'X';
+    figure[2][0] = 'X';
+    figure[2][1] = 'X';
+    figure[2][2] = 'X';
+}
 
+Glider::~Glider() {
+    for (int i = 0; i < GLIDER_SIZE; i++) {
+        delete[] figure[i];
+    }
+    delete[] figure;
+}
+
+Blinker::Blinker(int x, int y) {
+    xCoord = x;
+    yCoord = y;
+    height = BLINKER_HEIGHT;
+    width = BLINKER_WIDTH;
+    figure = new char* [BLINKER_HEIGHT];
+    for (int i = 0; i < BLINKER_HEIGHT; i++) {
+        figure[i] = new char[BLINKER_WIDTH];
+    }
+    for (int i = 0; i < BLINKER_HEIGHT; i++) {
+        for (int j = 0; j < BLINKER_WIDTH; j++) {
+            figure[i][j] = 'X';
+        }
+    }
+}
+
+Blinker::~Blinker() {
+    for (int i = 0; i < BLINKER_HEIGHT; i++) {
+        delete[] figure[i];
+    }
+    delete[] figure;
+}
+
+Dot::Dot(int x, int y) {
+    height = DOT_HEIGHT;
+    width = DOT_WIDTH;
+    xCoord = x;
+    yCoord = y;
+    figure = new char* [DOT_HEIGHT];
+    for (int i = 0; i < DOT_HEIGHT; i++) {
+        figure[i] = new char[DOT_WIDTH];
+    }
+    figure[0][0] = 'X';
+}
+
+Dot::~Dot()
+{
+    for (int i = 0; i < DOT_HEIGHT; i++) {
+        delete[] figure[i];
+    }
+    delete[] figure;
+}
+
+RandomField::RandomField() {
+    height = RandomField_HEIGHT;
+    width = RandomField_WIDTH;
+    xCoord = 0;
+    yCoord = 0;
+    figure = new char* [RandomField_HEIGHT];
+    for (int i = 0; i < RandomField_HEIGHT; i++) {
+        figure[i] = new char[RandomField_WIDTH];
+    }
+    for (int i = 0; i < RandomField_HEIGHT; i++) {
+        for (int j = 0; j < RandomField_WIDTH; j++) {
+            figure[i][j] = rand() % 2 == 0 ? 'X' : '.';
+        }
+    }
+}
+
+RandomField::~RandomField() {
+    for (int i = 0; i < RandomField_HEIGHT; i++) {
+        delete[] figure[i];
+    }
+    delete[] figure;
+}
 
 int main() {
+    //std::ios_base::sync_with_stdio(false);
+    SetWindow(WIDTH, HEIGHT+1);
 
     GameOfLife game = GameOfLife();
+    Glider g(0,0);
+    Blinker b(10, 10);
+
+    game.add(b);
+    game.add(g);
     game.run();
 
+    return 0;
 }
